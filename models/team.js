fcc192316e3f8de087ca4ae3eec12ff1e96c9d23
XONtech <xontech2k18@gmail.com>
2018-09-09 22:52:44 +0530

    this is done with handlebar and Brad

---
+++ models/team.js
@@ -0,0 +1,78 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
+
+var TeamSchema = mongoose.Schema({
+  team_name   : {type:String,required:false},
+  location    : {type:String,required:false},
+  date        : {type:String, required:false},
+  manager     : {
+    name        : {type:String,required:false},
+    number      : {type:String,required:false},
+    mail        : {type:String,required:false,index:true},
+    password    : {type:String,required:false}
+  },
+  captain     : {
+    name        : {type:String,required:false},
+    number      : {type:String,required:false},
+    mail        : {type:String,required:false}
+  },
+  players     : {
+    total       : {type:Number,required:false},
+    player_01   : {
+      name        : {type:String,required:false},
+      number      : {type:String,required:false}
+    },
+    player_02   : {
+      name        : {type:String,required:false},
+      number      : {type:String,required:false}
+    },
+    player_03   : {
+      name        : {type:String,required:false},
+      number      : {type:String,required:false}
+    },
+    player_04   : {
+      name        : {type:String,required:false},
+      number      : {type:String,required:false}
+    },
+    player_05   : {
+      name        : {type:String,required:false},
+      number      : {type:String,required:false}
+    },
+    player_06   : {
+      name        : {type:String,required:false},
+      number      : {type:String,required:false}
+    },
+    player_07   : {
+      name        : {type:String,required:false},
+      number      : {type:String,required:false}
+    },
+    player_08   : {
+      name        : {type:String,required:false},
+      number      : {type:String,required:false}
+    },
+    player_09   : {
+      name        : {type:String,required:false},
+      number      : {type:String,required:false}
+    },
+    player_10   : {
+      name        : {type:String,required:false},
+      number      : {type:String,required:false}
+    }
+  },
+  stats         : {
+    strikeRate    : {type:String, required:false},
+    wins          : {type:String, require:false}
+  }
+});
+
+
+var Team = module.exports = mongoose.model('Team', TeamSchema);
+
+module.exports.createTeam = (newTeam, callback) => {
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(newTeam.manager.password, salt, (err, hash)=>{
+      newTeam.manager.password = hash;
+      newTeam.save(callback);
+    })
+  })
+}
---
+++ routes/users.js
@@ -0,0 +1,89 @@
+var express = require('express');
+var router = express.Router();
+
+var Team = require('../models/team');
+
+//register route
+router.get('/register', (req, res)=>{
+  res.render('register');
+});
+
+// login route
+router.get('/login', (req, res)=>{
+  res.render('login');
+});
+
+
+// register route
+router.post('/register', (req, res)=>{
+
+
+  var team_name = req.body.team_name;
+  var location = req.body.location;
+  var dot = req.body.dot;
+  var name_manager = req.body.name_manager;
+  var num_manager = req.body.num_manager;
+  var mail_manager = req.body.mail_manager;
+  var password = req.body.password;
+  var password2 = req.body.password2;
+  var name_captain = req.body.name_captain;
+  var num_captain = req.body.num_captain;
+  var mail_captain = req.body.mail_captain;
+
+
+
+  // validation
+  req.checkBody('team_name', 'Team name is required').notEmpty();
+  req.checkBody('location', 'location is required').notEmpty();
+  req.checkBody('name_manager', 'A Manager name is required').notEmpty();
+  req.checkBody('num_manager', 'Manager contact number is required').notEmpty();
+  req.checkBody('mail_manager', 'Manager mail address is required').notEmpty();
+  req.checkBody('mail_manager', 'Email address is invalid/ not correct').isEmail();
+  req.checkBody('name_captain', 'A Captain Name is required').notEmpty();
+  req.checkBody('password', 'Password is required').notEmpty();
+  req.checkBody('password2', 'Password do not match').equals(req.body.password);
+
+  var errors = req.validationErrors();
+
+  if(errors){
+    res.render('register', {
+      errors : errors
+    })
+  }
+  else {
+    Team.findOne({ 'manager.mail' : mail_manager }, function(err, team) {
+      if (err) throw err;
+      if (team) {
+        req.flash('error_msg', 'That manager email is already registered.');
+        res.redirect('/users/register');
+        console.log("exist");
+      }
+      else {
+
+        var newTeam = new Team();
+
+        newTeam.team_name = team_name,
+        newTeam.location = location,
+        newTeam.date = dot,
+        newTeam.manager.name = name_manager,
+        newTeam.manager.number = num_manager,
+        newTeam.manager.mail = mail_manager,
+        newTeam.manager.password = password,
+        newTeam.captain.name = name_captain,
+        newTeam.captain.number = num_captain,
+        newTeam.captain.mail = mail_captain
+
+        Team.createTeam(newTeam, (err, user) => {
+          if(err) throw err;
+          console.log(user);
+        });
+
+        req.flash('success_msg', 'you Team has been successfully registered');
+        res.redirect('/users/login');
+      }
+    });
+  };
+
+});
+
+module.exports = router;
