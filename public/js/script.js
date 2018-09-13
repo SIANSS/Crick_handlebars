72d1bcb1cf22503da617afc7a8a534ee56816134
XONtech <xontech2k18@gmail.com>
2018-09-13 12:25:42 +0530

    udpated again

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,39 @@
-$(function () {
-  $("#datepicker").datepicker({
-        autoclose: true,
-        todayHighlight: true
-  }).datepicker('update', new Date());
-});
+var getUrl = window.location;
+var baseUrl = getUrl .protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
+console.log(baseUrl);
+
+var datus = document.getElementById('datus');
+datus.min = new Date().toISOString().split("T")[0];
+
+function getallteams(){
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var data = JSON.parse(this.responseText);
+      console.log(data.length);
+      selectEl = document.getElementById('row');
+      selectEl.classList.remove("hidden");
+
+      for(var i = 0; i < data.length; i++){
+        selectEl.options.add(new Option(data[i].team_name, data[i].team_name));
+      }
+
+      document.getElementById('btn').classList.remove("hidden");
+      document.getElementById('btn').innerHTML = "Select";
+
+
+      document.addEventListener('click',function(e){
+        if(e.target && e.target.id== 'btn'){
+          selected();
+        }
+      })
+    }
+  };
+  xhttp.open("GET", baseUrl+"/getallteam", true);
+  xhttp.send();
+}
+
+function selected(){
+  // console.log("hellow");
+  document.getElementById('selection-section').innerHTML = "<br><div class='form-group'><label>Select Play Time :</label><input class='form-control' type='time'></div><div class='form-group'><label>Maximum Overs</label><select class='form-control'><option value='5'>5</option><option value='6'>6</option><option value='10'>10</option><option value='20'>20</option></select></div>";
+}
--- routes/matches.js
+++ routes/matches.js
@@ -1,3 +1,7 @@
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/crick');
+var db = mongoose.connection;
 var express = require('express');
 var router = express.Router();
 
@@ -16,6 +20,20 @@ function ensureAuthenticated(req, res, next){
 		res.redirect('/users/login');
 	}
 }
+
+router.get('/getallteam', ensureAuthenticated, (req, res)=> {
+
+  db.collection('teams').find({}).toArray((err, result) =>{
+    if (err) throw err;
+    if (!result) {
+      console.log("no result avaialble!!!");
+    }
+    else {
+      res.send(result);
+      // console.log("This is result :"+result);
+    }
+  });
+});
 
 
 module.exports = router;
